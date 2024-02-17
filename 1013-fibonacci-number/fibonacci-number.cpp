43694fae3c9d347bcb093b0765a94279023345ba
Shiva Patel <158433560+shivapatel7206@users.noreply.github.com>
2024-02-17 13:43:52 +0530

    Time: 3 ms (61.94%) | Memory: 7.1 MB (34.19%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int fib(int n) {
+        int fb[35];
+        fb[0]=0;
+        fb[1]=1;
+        for(int i=2;i<35;i++) fb[i]=fb[i-1]+fb[i-2];
+        return fb[n];
+    }
+};
