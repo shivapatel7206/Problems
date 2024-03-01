d8e327f3834133d322cd0e9ed4c45b6d53994633
Shiva Patel <158433560+shivapatel7206@users.noreply.github.com>
2024-03-02 03:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (5.63%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left =0;
+        int right = nums.size()-1;
+        //Binary Search Algo
+        while(left<= right){
+            int mid = left + (right-left)/2;
 
-   int searchInsert(vector<int>& nums, int target) {
-       int start=0;
-       int end=nums.size()-1;
-       while(start<=end){
-          int mid=(start+end)/2;
-           if(nums[mid]==target)
-           return mid;
-           else if(target<nums[mid]){
-               end=mid-1;
-           }
-           else
-           start=mid+1;
-       }
-       return end+1;
-   }    
+            // if target is found return its index
+            if(nums[mid] == target) return mid;
+
+            else if(nums[mid] > target) right = mid-1;
+            else left = mid+1;
+
+        }
+        //returning left index as it contains the position where it would be if it were inserted in order.
+        return left;
+    }
 };
