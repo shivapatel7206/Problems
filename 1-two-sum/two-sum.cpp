dcc85385d44a78ecf14ecde39588e8fa39c14b27
Shiva Patel <158433560+shivapatel7206@users.noreply.github.com>
2024-03-11 23:34:01 +0530

    Time: 50 ms (36.96%) | Memory: 12.7 MB (61.84%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,18 @@
+#include <vector>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
                 if (nums[i] + nums[j] == target) {
                     return {i, j};
                 }
             }
         }
+        
         return {};
     }
 };
