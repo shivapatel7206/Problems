1b6509e2a932747b74fac63a78d23609a415ed73
Shiva Patel <158433560+shivapatel7206@users.noreply.github.com>
2024-03-23 23:20:27 +0530

    Time: 6 ms (22.98%) | Memory: 8.1 MB (91.23%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
-    int myAtoi(string s) 
-    {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
+    int myAtoi(string s) {
+        int n = s.size();
+        double res = 0;
+        bool dig = false;
+        bool posi = false;
+        bool neg = false;
+        bool other = false;
+
+        int i = 0;
+        while (i < n && s[i] == ' ')
             i++;
-        if(s[i]=='-')
-        {
-            sign=-1;
-            i++;
-        }
-        else if(s[i]=='+')
-            i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
-                i++;
+
+        for (; i < n; i++) {
+            if (other)
+                break;
+            
+            if (s[i] >= '0' && s[i] <= '9') {
+                res = (res * 10) + (s[i] - '0');
+                dig = true;
+            } else {
+                if (dig)
+                    break;
+
+                else if (s[i] == '-' && !neg)
+                    neg = true;
+
+                else if (s[i] == '+' && !posi)
+                    posi = true;
+
+                else
+                    other = true;
             }
-            else
-                return ans*sign;
         }
-        return (ans*sign);
+
+        if (posi && neg)
+            return 0;
+
+        if (neg)
+            res = -res;
+
+        if (res > INT_MAX)
+            return INT_MAX;
+
+        if (res < INT_MIN)
+            return INT_MIN;
+
+        return (int)res;
     }
 };
